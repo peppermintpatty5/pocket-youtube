f5e8c2acf4bef9a2c8632e05caa6fcc71096c513
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-07-18 20:53:16 -0400

    Added channel relation to database

--- init_db.php
+++ init_db.php
@@ -1,40 +1,68 @@
 <?php
 require_once "mysql.php";
 
-const VIDEO_DIR = "videos";
-
 $mysqli = new mysqli($hostname, $username, $password, $database);
 
-$mysqli->query("DROP TABLE IF EXISTS video");
+// recreate all tables
+$mysqli->query("DROP TABLE IF EXISTS channel, video");
 $mysqli->query(
-    "CREATE TABLE video(
-        id VARCHAR(15) PRIMARY KEY,
-        title VARCHAR(255),
-        description TEXT,
-        ext VARCHAR(7)
+    "CREATE TABLE channel(
+        id varchar(24) PRIMARY KEY,
+        uploader varchar(255)
     ) COLLATE=utf8mb4_0900_bin"
 );
-$stmt = $mysqli->prepare(
-    "INSERT INTO video(
-        id, title, description, ext
+$mysqli->query(
+    "CREATE TABLE video(
+        id varchar(15) PRIMARY KEY,
+        title varchar(255),
+        description text,
+        channel_id varchar(24),
+        ext varchar(7),
+        FOREIGN KEY (channel_id) REFERENCES channel(id)
+    ) COLLATE=utf8mb4_0900_bin"
+);
+
+// prepared INSERT statements
+$channel_insert = $mysqli->prepare(
+    "INSERT INTO channel(
+        id, uploader
     ) VALUES(
-        ?, ?, ?, ?
+        ?, ?
+    )"
+);
+$video_insert = $mysqli->prepare(
+    "INSERT INTO video(
+        id, title, description, channel_id, ext
+    ) VALUES(
+        ?, ?, ?, ?, ?
     )"
 );
 
-$files = scandir(VIDEO_DIR);
+$files = scandir("videos");
 foreach ($files as $file) {
     if (preg_match("/^.*\.info\.json$/", $file)) {
-        $json = file_get_contents(VIDEO_DIR . "/$file");
+        $json = file_get_contents("videos/{$file}");
         $data = json_decode($json, true);
 
-        $stmt->bind_param(
-            "ssss",
+        $channel_insert->bind_param(
+            "ss",
+            $data["channel_id"],
+            $data["uploader"]
+        );
+        $video_insert->bind_param(
+            "sssss",
             $data["id"],
             $data["title"],
             $data["description"],
+            $data["channel_id"],
             $data["ext"]
         );
-        $stmt->execute();
+
+        /**
+         * Must insert into to channel before video to meet foreign key
+         * constraint on video.channel_id
+         */
+        $channel_insert->execute();
+        $video_insert->execute();
     }
 }
