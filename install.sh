ad95f5718f7cffc19f3dd479e2c83132f04b8b98
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-08-08 11:19:06 -0400

    Use symlink instead of realpath in Apache config

--- install.sh
+++ install.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-if [ -L videos ]; then
-    video_dir=$(realpath videos)
+if [ $USER != root ]; then
+    echo "$0: must be root"
+    exit 1
+fi
 
-    if [ -d "$video_dir" ]; then
-        export video_dir
-
-        envsubst < server/local-youtube.conf |
-            tee /etc/apache2/conf-available/local-youtube.conf
-        a2enconf local-youtube
-        systemctl reload apache2
-    else
-        echo "$0: 'videos' must refer to a directory"
-    fi
+if [ -d videos ]; then
+    envsubst \
+        < server/local-youtube.conf \
+        > /etc/apache2/conf-available/local-youtube.conf
+    a2enconf local-youtube
+    systemctl reload apache2
 else
-    echo "$0: missing symlink 'videos'"
+    echo "$0: missing directory 'videos'"
 fi
