d0953471bf7cc67888bc67b1d9a82f91e5d0e400
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2021-08-12 12:13:43 -0400

    Virtual host test configuration

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ fi
 if [ -d videos ]; then
     envsubst \
         < server/local-youtube.conf \
-        > /etc/apache2/conf-available/local-youtube.conf
-    a2enconf local-youtube
+        > /etc/apache2/sites-available/local-youtube.conf
+    a2ensite local-youtube
     systemctl reload apache2
 else
     echo "$0: missing directory 'videos'"
